979acf3c05506a3462d707b8209775a07f96cdbe
Matt Blackburn <mblack88@ford.com>
2017-03-31 13:29:03 -0400

    Added iTunes stuff

--- killprogs.bat
+++ killprogs.bat
@@ -3,3 +3,6 @@ taskkill /f /IM outlook.exe
 taskkill /f /IM Lync.exe
 taskkill /f /IM Evernote.exe
 taskkill /f /IM Chrome.exe
+taskkill /f /IM iTunesHelper.exe
+taskkill /f /IM iTunes.exe
+taskkill /f /IM BtITunesPlugIn.exe
